b94cd5b7f5f01276e5577d8c6cb43d79cc51aa90
Abhishek Rai <abhishekr.hestabit@gmail.com>
2026-01-08 11:13:50 +0530

    Fix: correct vegetable from Tomato to Potato

--- fruits.js
+++ fruits.js
@@ -2,7 +2,7 @@ console.log("Fruits list:");
 console.log("Apple");
 console.log("Banana");
 console.log("Mango");
-console.log("Potato"); // BUG: should be Tomato
+console.log("Tomato"); // BUG: should be Tomato
 console.log("Orange");
 console.log("Grapes");
 console.log("Pineapple");
